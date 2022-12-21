43d8f5ea7bc70b0bb47a956ec25d1f033c5f74fc
Jonatas Batista Mendonca Freitas <jonatas.freitas@localiza.com>
2022-12-21 08:03:52 -0300

    Commit Inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/img/powered.png';
 
 function App() {
+  const [altura, definirAltura] = useState<number>(0);
+  const [peso, definirPeso] = useState<number>(0);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header className={styles.headerContainer}>
+        <img src={poweredImage} alt='Powered' width={150} />
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          
+          <h1>Calcule seu IMC.</h1>
+          <p>IMC é a sigla para índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de saúde para calcular o peso ideal de cada pessoa.</p>
+          
+          <input type='number'
+            placeholder="Digite a sua altura. Ex.: 1,5 (em metros)"
+            value={altura > 0 ? altura : ''}
+            onChange={e => definirAltura(parseFloat(e.target.value))}
+          />
+          
+          <input type='number'
+            placeholder="Digite o seu peso. Ex. 82,5 (em kg)"
+            value={peso > 0 ? peso : ''}
+            onChange={e => definirPeso(parseFloat(e.target.value))}
+          />
+        </div>
+        <div className={styles.rightSide}>
+          aqui
+        </div>
+      </div>
+
     </div>
   );
 }
