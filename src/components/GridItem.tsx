72275040acd53f41249537cd5959b1471068feff
Jonatas Batista Mendonca Freitas <jonatas.freitas@localiza.com>
2022-12-22 08:39:45 -0300

    Estilos e preenchimento do lado direito

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,11 +1,26 @@
 import { Nivel } from "../helpers/imc";
+import styles from './gridItem.module.css';
+import upImg from '../assets/img/up.png';
+import downImg from '../assets/img/down.png';
 
-type Props ={
+type Props = {
     item: Nivel
 }
 
-export const GridItem = ({item}: Props) => {
+export const GridItem = ({ item }: Props) => {
     return (
-        <div>...</div>
+        <div className={styles.main} style={{ backgroundColor: item.color }}>
+            <div className={styles.gridIcone}>
+                <img src={item.icone === 'up' ? upImg : downImg} alt="" width="30" />
+            </div>
+            <div className={styles.gridTitulo}>
+                {item.titulo}
+            </div>
+            <div className={styles.gridInfo}>
+                <>
+                    IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+                </>
+            </div>
+        </div>
     );
 }
